cdb2a35b4456602f1d083aeaa2397ec5c6257a83
innoblue2019 <55012605+innoblue2019@users.noreply.github.com>
2025-12-02 23:05:07 +0800

    Set site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://innoblue2019.github.io',
+  base: '/devprotfolio',
   vite: {
     plugins: [tailwindcss()],
   },
